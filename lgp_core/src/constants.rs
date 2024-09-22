59aaf0798b762786d25c5b3eb827fb7159903ee5
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-22 17:49:16 -0500

    Create constants file

---
+++ lgp_core/src/constants.rs
@@ -0,0 +1,10 @@
+pub const NUM_OPS: u8 = 4;
+pub const NUM_VAR_REG: u8 = 3;
+pub const NUM_CON_REG: u8 = 255;
+
+pub const OPCODE_ADD: u8 = 0;
+pub const OPCODE_SUB: u8 = 1;
+pub const OPCODE_MUL: u8 = 2;
+pub const OPCODE_DIV: u8 = 3;
+
+pub type Instruction = u32;

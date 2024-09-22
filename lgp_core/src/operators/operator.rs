b436075fa7cea5939315efb122ab6abe768a1f3d
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-22 18:15:15 -0500

    Define Operator trait and specify apply() method signature

---
+++ lgp_core/src/operators/operator.rs
@@ -0,0 +1,7 @@
+use crate::program::registers::Registers;
+
+
+// Each of the operators in the programming language will implement the Operator trait
+pub trait Operator {
+    fn apply(&self, operands: Vec<f32>, result: &mut f32);
+}

4524a5b0b47d39ec4f59d3f8ad82ddd845f4b017
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-23 20:49:46 -0500

    Test with main

--- lgp/src/main.rs
+++ lgp/src/main.rs
@@ -1,3 +1,10 @@
+use lgp_core::program::Program;
+
 fn main() {
-    println!("Hello, world!");
+    let min_length: usize = 8;
+    let max_length: usize = 16;
+    let mut prog: Program = Program::new_random(min_length, max_length);
+
+    println!("Program: {:?}", prog.instructions);
+    println!("Constants: {:?}", prog.registers.constants);
 }

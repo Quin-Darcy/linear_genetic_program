0c40869a40b8d5342cd881f3dd73d9fab146ef82
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-23 20:48:37 -0500

    Update inputs and outputs

--- lgp_core/src/constants.rs
+++ lgp_core/src/constants.rs
@@ -8,5 +8,7 @@ pub const OPCODE_MUL: u8 = 2;
 pub const OPCODE_DIV: u8 = 3;
 
 pub const DIV_UNDEF: f32 = f32::MAX;
+pub const NUM_INPUTS: usize = 1;
+pub const NUM_OUTPUTS: usize = 1;
 
 pub type Instruction = u32;

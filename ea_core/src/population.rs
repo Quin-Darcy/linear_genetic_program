34cfc457c9df31ce9a8085d59376fa3923ae6b30
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-26 22:25:14 -0500

    Add more members; add evaluate method to express training input through each genotype

--- ea_core/src/population.rs
+++ ea_core/src/population.rs
@@ -1,12 +1,20 @@
+use lgp_core::constants::{NUM_INPUTS, NUM_OUTPUTS};
 use crate::genotype::Genotype;
 
 
 pub struct Population {
     pub genotypes: Vec<Genotype>,
+    pub population_size: usize,
+    pub training_set: Vec<([f32; NUM_INPUTS], [f32; NUM_OUTPUTS])>
 }
 
 impl Population {
-    pub fn new(population_size: usize, min_length: usize, max_length: usize) -> Self {
+    pub fn new(
+        population_size: usize, 
+        min_length: usize, 
+        max_length: usize,
+        training_set: Vec<([f32; NUM_INPUTS], [f32; NUM_OUTPUTS])>
+    ) -> Self {
         let mut genotypes: Vec<Genotype> = Vec::with_capacity(population_size);
 
         for _ in 0..population_size {
@@ -15,7 +23,21 @@ impl Population {
         }        
 
         Population {
-            genotypes
+            genotypes,
+            population_size,
+            training_set
         }
+    }
+
+    pub fn evaluate(&mut self) -> Vec<[f32; NUM_OUTPUTS]> {
+        let mut results: Vec<[f32; NUM_OUTPUTS]> = Vec::with_capacity(self.population_size);
+
+        for i in 0..self.population_size {
+            for j in 0..self.training_set.len() {
+                results.push(self.genotypes[i].express(&self.training_set[j].0));
+            }
+        }
+
+        results
     }
 }

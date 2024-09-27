53269b419ddaba0673960539224f812528bae2c6
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-26 22:25:38 -0500

    Update main to test new additions

--- lgp/src/main.rs
+++ lgp/src/main.rs
@@ -1,4 +1,3 @@
-use lgp_core::program::Program;
 use lgp_core::constants::{NUM_INPUTS, NUM_OUTPUTS};
 use ea_core::population::Population;
 
@@ -6,6 +5,10 @@ fn main() {
     let min_length: usize = 8;
     let max_length: usize = 16;
     let population_size: usize = 10;
+    let training_set: Vec<([f32; NUM_INPUTS], [f32; NUM_OUTPUTS])> = vec![([1.0], [1.0])];
 
-    let mut pop = Population::new(population_size, min_length, max_length);
+    let mut pop = Population::new(population_size, min_length, max_length, training_set);
+    let results: Vec<[f32; NUM_OUTPUTS]> = pop.evaluate();
+
+    println!("{:?}", results);
 }

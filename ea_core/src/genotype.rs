d3d3c87ea7cc7b3aa5cc57edbfdb98a0eec1ddf5
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-09-28 00:38:41 -0500

    Add fitness method

--- ea_core/src/genotype.rs
+++ ea_core/src/genotype.rs
@@ -4,7 +4,8 @@ use lgp_core::constants::{NUM_INPUTS, NUM_OUTPUTS};
 
 pub struct Genotype {
     pub program: Program,
-    pub fitness: Option<f32>
+    pub fitness: Option<f32>,
+    pub output: Option<[f32; NUM_OUTPUTS]>
 }
 
 impl Genotype {
@@ -13,11 +14,21 @@ impl Genotype {
 
         Genotype {
             program: prog,
-            fitness: None
+            fitness: None,
+            output: None
         }
     }
 
-    pub fn express(&mut self, input: &[f32; NUM_INPUTS]) -> [f32; NUM_OUTPUTS] {
-        self.program.run(input)
+    pub fn express(&mut self, input: &[f32; NUM_INPUTS]) {
+        self.output = Some(self.program.run(input));
     }
+
+    pub fn set_fitness(&mut self, expected: &[f32; NUM_OUTPUTS]) {
+        self.fitness = Some(expected.iter()
+            .zip(self.output.unwrap().iter())
+            .map(|(a, b)| (a - b).powf(2.0))
+            .sum::<f32>() / (expected.len() as f32));
+    }
+
+    // TODO: Add mutation method    pub
 }
